5d89cd030092e4e7ebc53e420c8686d4b8a9717d
Alex Schwimmer <schwimmeralexander@gmail.com>
2023-09-29 22:20:09 -0700

    Began working on Project 1

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def css(request):
+    
+    print(util.get_entry(request))
+    return render(request, "encyclopedia/index.html")
